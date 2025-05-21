59a04cd3d494b1a9b978d080eebf28a740c00500
Algol95 <angelaragon.developer@gmail.com>
2025-05-21 09:00:07 +0200

    refactor(TL-13): Refactor task controller

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -1,33 +1,37 @@
 from models import Task
 from sqlalchemy.orm import Session
 
-def createTask(db:Session, title:str, description:str, state_id:int, user_id:int):
-    task = Task(title = title, description = description, state_id = state_id, user_id = user_id)
+def createTask(db: Session, title: str, description: str, state_id: int, user_id: int):
+    task = Task(title=title, description=description, state_id=state_id, user_id=user_id)
     db.add(task)
     db.commit()
     db.refresh(task)
     return task
 
-def getAllTasks(db:Session):
+def getAllTasks(db: Session):
     return db.query(Task).all
 
-def getTaskById(db:Session, task_id:int):
+def getTaskById(db: Session, task_id: int):
     return db.query(Task).filter(Task.id == task_id).first
 
-def updateTask(db:Session, task_id:int, new_title:str, new_description:str, new_state:int):
+def updateTask(db: Session, task_id: int, new_title: str, new_description: str, new_state: int):
     task = getTaskById(db, task_id)
-    if not task: return None
-    if new_title: task.title = new_title
-    if new_description: task.description = new_description
-    if new_state: task.state = new_state
+    if not task:
+        return None
+    if new_title:
+        task.title = new_title
+    if new_description:
+        task.description = new_description
+    if new_state:
+        task.state = new_state
     db.commit()
     db.refresh(task)
     return task
 
-def deleteTask(db:Session, task_id:int):
+def deleteTask(db: Session, task_id: int):
     task = getTaskById(db, task_id)
-    if not task: return False
+    if not task:
+        return False
     db.delete(task)
     db.commit()
     return True
-    
