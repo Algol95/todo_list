be4d8e81cdd4a7f3001dc09d5a6a70d937cc1021
Aelnordev <loreriba99@gmail.com>
2025-05-23 09:45:59 +0200

    docs(TL-23): Document UserController and StateController

--- controllers/state_controller.py
+++ controllers/state_controller.py
@@ -2,8 +2,32 @@ from models import State
 from sqlalchemy.orm import Session
 
 class StateController():
+    """
+    Controlador para operaciones relacionadas con el modelo State.
+
+    Author:
+        Lorena Martínez
+        Ángel Aragón
+
+    Methods:
+        **createState**(db, name): Crea estado nuevo
+        **findStateByName**(db, name): Busca el estado por nombre de estado
+        
+    """
 
     def createState(db: Session, name: str):
+        """Crea estado nuevo
+
+        Args:
+            db (Session): Sesión activa de SQLAlchemy
+            name (str): Nombre de estado
+
+        Raises:
+            Exception: El nombre de usuario no puede estar vacío
+
+        Returns:
+            State/str: Objeto State / mensaje de error
+        """
         try:
             if not name:
                 raise Exception("El nombre del estado no puede estar vacío")
@@ -17,6 +41,18 @@ class StateController():
             return f"\033[31m❌ {e}\033[0m"
 
     def findStateByName(db: Session, name: str):
+        """Busca el estado por nombre de estado
+
+        Args:
+            db (Session): Sesión activa de SQLAlchemy
+            name (str): Nombre de estado
+
+        Raises:
+            Exception: El nombre de usuario no puede estar vacío
+
+        Returns:
+            State/str: Objeto State / mensaje de error
+        """
         try:
             if not name:
                 raise Exception("El nombre del estado no puede estar vacío")
--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -2,8 +2,32 @@ from sqlalchemy.orm import Session
 from models import User
 
 class UserController():
+    """
+    Controlador para operaciones relacionadas con el modelo User.
 
+    Author:
+        Lorena Martínez
+        Ángel Aragón
+
+    Methods:
+        **findUserByUsername**(db, usename): Busca el usuario por nombre de usuario
+        **createUser**(db, username, password): Crea usuario nuevo
+        
+    """
     def findUserByUsername(db: Session, username: str):
+        """Busca el usuario por nombre de usuario
+
+        Args:
+            db (Session): Sesión activa de SQLAlchemy
+            username (str): Nombre de usuario
+
+        Raises:
+            Exception: El nombre de usuario no puede estar vacío
+            Exception: El usuario no existe
+
+        Returns:
+            User/str: Objeto User / mensaje de error
+        """
         try:
             if not username:
                 raise Exception("El nombre de usuario no puede estar vacío")
@@ -15,6 +39,19 @@ class UserController():
             return f"\033[31m❌ {e}\033[0m"
 
     def createUser(db: Session, username: str, password: str):
+        """Crea usuario nuevo
+
+        Args:
+            db (Session): Sesión activa de SQLAlchemy
+            username (str): Nombre de usuario
+            password (str): Contraseña
+
+        Raises:
+            Exception: El nombre de usuario y la contraseña no pueden estar vacíos
+
+        Returns:
+            User/str: Objeto User / mensaje de error
+        """
         try:
             if not username or not password:
                 raise Exception("El nombre de usuario y la contraseña no pueden estar vacíos")
